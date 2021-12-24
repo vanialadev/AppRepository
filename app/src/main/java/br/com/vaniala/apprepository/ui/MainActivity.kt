d72ad96786b07e9fe56e4fdfd67f0c955498d480
Vania <vania.almeida28@hotmail.com>
2021-12-24 17:36:36 -0300

    menu

--- app/src/main/java/br/com/vaniala/apprepository/ui/MainActivity.kt
+++ app/src/main/java/br/com/vaniala/apprepository/ui/MainActivity.kt
@@ -2,10 +2,15 @@ package br.com.vaniala.apprepository.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import br.com.vaniala.apprepository.R
+import br.com.vaniala.apprepository.core.hideSoftKeyboard
 import br.com.vaniala.apprepository.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -17,5 +22,24 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
+    }
 }
